c8e5907adee08e86bb52aca34fec39883fc8e750
Heritiana Andrianjafison <h.andrianjafison@gmail.com>
2026-03-06 15:39:13 +0300

    Assfsqsqssqsq

--- src/main/java/mg/jovena/test/controller/TestController.java
+++ src/main/java/mg/jovena/test/controller/TestController.java
@@ -15,4 +15,9 @@ public class TestController {
     public String sayYes() {
         return "YESSS !! ";
     }
+
+    @GetMapping("/no")
+    public String sayNo() {
+        return "Noooo !! ";
+    }
 }
