485bcfecfbee218355a9726db40f56a68a38c603
Heritiana Andrianjafison <h.andrianjafison@gmail.com>
2026-03-09 14:11:02 +0300

    Hello Heriitana

---
+++ src/main/java/mg/jovena/test/controller/HelloControlleur.java
@@ -0,0 +1,12 @@
+package mg.jovena.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControlleur {
+    @GetMapping("/go")
+    public String helloWorld() {
+        return "Hello Heritiana";
+    }
+}
